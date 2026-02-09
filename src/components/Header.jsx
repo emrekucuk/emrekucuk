b7aa2efef3d214cfb55a9c7b80e17e6b8db9a33f
Emre Küçük <emrekucuk74@hotmail.com>
2026-02-10 00:10:05 +0300

    Updated jobs

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,8 +7,8 @@ export default function Header() {
       <p>Software Team Lead</p>
       <nav>
         <Link to="/">Anasayfa</Link>
-        <Link to="/projects">Projeler</Link>
         <Link to="/jobs">İşler</Link>
+        <Link to="/projects">Projeler</Link>
         <Link to="/skills">Beceriler</Link>
         <Link to="/educations">Eğitim</Link>
       </nav>
--- src/pages/Jobs.jsx
+++ src/pages/Jobs.jsx
@@ -3,12 +3,12 @@ import JobCard from "../components/JobCard";
 export default function Jobs() {
   const jobs = [
     {
-      title: "Tarım Kredi Teknoloji | Team Lead | Mayıs 2023 - Eylül 2024",
+      title: "Tarım Kredi Teknoloji | Team Lead | Eylül 2024 - Devam Ediyor",
       description: "KoopEnerji ve KoopPOS projelerinde hem geliştirici hem de takım lideri olara kgörev almaktayım",
       tech: "Dotnet, MsSql, Mikroservice, MongoDb, Kafka, Docker, Jenkins, Kubernetes, Jira, Gitlab, Serilog, Graylog, WebSocket",
     },
     {
-      title: "Tarım Kredi Teknoloji | Software Developer |Mayıs 2023 - Eylül 2024",
+      title: "Tarım Kredi Teknoloji | Software Developer | Mayıs 2023 - Eylül 2024",
       description: "Birden fazla projede aktif olarak geliştirici rolü üstlendim. KoopEnerji projesinin sıfırdan yazılmasında ve Toprak platformunda mikroservis mimarisinde bir çok modülde görev aldım.",
       tech: "Dotnet, MsSql, Mikroservis, MongoDb, Kafka, Docker, Jenkins, Kubernetes, Jira, Gitlab, Serilog, Graylog, WebSocket",
     },
